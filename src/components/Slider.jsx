fbffc8e05d5aa633deda2c51fee467fcaba3491c
Cyril-vtn <votion.cyril@gmail.com>
2024-06-09 10:59:28 +0200

    chore: Add condition to display navigation arrows

--- src/components/Slider.jsx
+++ src/components/Slider.jsx
@@ -5,11 +5,11 @@ export const Slider = ({ images }) => {
   const [currentImageIndex, setCurrentImageIndex] = useState(0);
 
   const handleChangeImg = (direction) => {
-    if (direction === 'left') {
+    if (direction === "left") {
       setCurrentImageIndex((oldIndex) => {
         return oldIndex === 0 ? images.length - 1 : oldIndex - 1;
       });
-    } else if (direction === 'right') {
+    } else if (direction === "right") {
       setCurrentImageIndex((oldIndex) => {
         return oldIndex === images.length - 1 ? 0 : oldIndex + 1;
       });
@@ -17,9 +17,23 @@ export const Slider = ({ images }) => {
   };
 
   return (
-    <div className="slider" style={{ backgroundImage: `url(${images[currentImageIndex]})` }}>
-      <MdArrowBackIosNew size={79} className="arrow_left" onClick={() => handleChangeImg('left')} />
-      <MdArrowForwardIos className="arrow_right" onClick={() => handleChangeImg('right')} />
+    <div
+      className="slider"
+      style={{ backgroundImage: `url(${images[currentImageIndex]})` }}
+    >
+      {images.length > 1 && (
+        <>
+          <MdArrowBackIosNew
+            size={79}
+            className="arrow_left"
+            onClick={() => handleChangeImg("left")}
+          />
+          <MdArrowForwardIos
+            className="arrow_right"
+            onClick={() => handleChangeImg("right")}
+          />
+        </>
+      )}
     </div>
   );
 };
