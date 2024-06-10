94e7c2473d8eb47f7ddc42598594c6783a5f0296
Cyril-vtn <votion.cyril@gmail.com>
2024-06-10 17:34:01 +0200

    refactor: Format document and fix indentation

--- src/Pages/About.jsx
+++ src/Pages/About.jsx
@@ -1,5 +1,5 @@
 import { OverlayCard } from "../components/OverlayCard";
-import img from '../assets/about-background.png';
+import img from "../assets/about-background.png";
 import "../styles/about.scss";
 import { Dropdown } from "../components/Dropdown";
 
@@ -8,10 +8,22 @@ export default function About() {
     <div className="about_container">
       <OverlayCard image={img} />
       <div className="about_content">
-        <Dropdown label="Fiabilité" description="Les annonces postées sur Kasa garantissent une fiabilité totale. Les photos sont conformes aux logements, et toutes les informations sont régulièrement vérifiées  par nos équipes." />
-        <Dropdown label="Respect" description="La bienveillance fait partie des valeurs fondatrices de Kasa. Tout comportement discriminatoire ou de perturbation du voisinage entraînera une exclusion de notre plateforme." />
-        <Dropdown label="Service" description="La bienveillance fait partie des valeurs fondatrices de Kasa. Tout comportement discriminatoire ou de perturbation du voisinage entraînera une exclusion de notre plateforme." />
-        <Dropdown label="Sécurité" description="La sécurité est la priorité de Kasa. Aussi bien pour nos hôtes que pour les voyageurs, chaque logement correspond aux critères de sécurité établis par nos services. En laissant une note aussi bien à l'hôte qu'au locataire, cela permet à nos équipes de vérifier que les standards sont bien respectés. Nous organisons également des ateliers sur la sécurité domestique pour nos hôtes." />
+        <Dropdown
+          label="Fiabilité"
+          description="Les annonces postées sur Kasa garantissent une fiabilité totale. Les photos sont conformes aux logements, et toutes les informations sont régulièrement vérifiées  par nos équipes."
+        />
+        <Dropdown
+          label="Respect"
+          description="La bienveillance fait partie des valeurs fondatrices de Kasa. Tout comportement discriminatoire ou de perturbation du voisinage entraînera une exclusion de notre plateforme."
+        />
+        <Dropdown
+          label="Service"
+          description="La bienveillance fait partie des valeurs fondatrices de Kasa. Tout comportement discriminatoire ou de perturbation du voisinage entraînera une exclusion de notre plateforme."
+        />
+        <Dropdown
+          label="Sécurité"
+          description="La sécurité est la priorité de Kasa. Aussi bien pour nos hôtes que pour les voyageurs, chaque logement correspond aux critères de sécurité établis par nos services. En laissant une note aussi bien à l'hôte qu'au locataire, cela permet à nos équipes de vérifier que les standards sont bien respectés. Nous organisons également des ateliers sur la sécurité domestique pour nos hôtes."
+        />
       </div>
     </div>
   );
